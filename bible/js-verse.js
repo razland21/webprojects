ee010bd658640880478ae9500dc92e7906049c49
Rica <razland21@gmail.com>
2017-06-28 17:41:39 -0700

    API integration v1

---
+++ bible/js-verse.js
@@ -0,0 +1,33 @@
+function parse_verse(my_verse) {
+    var source = "- "
+    var full_verse = ""
+
+    for(var i=0; i<my_verse.length; i++) {
+        full_verse += "<p>" + my_verse[i].text + " </p>";
+        if (i==0) {
+            source += my_verse[i].bookname + " " + my_verse[i].chapter + ":" + my_verse[i].verse;
+        } else if (i==(my_verse.length-1)) {
+            source += "-" + my_verse[i].verse;
+        } 
+    }
+
+    $("#vtext").html(full_verse);
+    $("#vloc").html(source);
+}
+
+
+function get_votd() {
+    $.ajax({
+        url: "http://labs.bible.org/api/?",
+        dataType:"jsonp",
+        data: "passage=votd&type=json&callback=?",
+        success: function(my_verse) {
+            parse_verse(my_verse);
+        }
+    });
+}
+
+
+$("document").ready(function(){
+    get_votd();
+});
